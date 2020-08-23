aa68a5239c23cc569e9c4b5731e386fd6e7d34fa
Mohammad Zakariya <mohdzakariya272@gmail.com>
2020-08-23 14:06:35 +0530

    Hello World

---
+++ app/src/main/java/com/zakariya/foodchef/model/FoodItem.kt
@@ -0,0 +1,3 @@
+package com.zakariya.foodchef.model
+
+data class FoodItem(val id: String?, val name: String?, val cost: Int?)
---
+++ app/src/main/java/com/zakariya/foodchef/model/OrderDetails.kt
@@ -0,0 +1,10 @@
+package com.zakariya.foodchef.model
+
+import org.json.JSONArray
+
+data class OrderDetails(
+    val orderId: Int,
+    val resName: String,
+    val orderDate: String,
+    val foodItem: JSONArray
+)
